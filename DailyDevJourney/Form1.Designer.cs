b9af45e9cbcc7aaf37918288ca35ea7048b47045
Alain <Alain@FIXEBUREAU>
2026-01-22 09:26:16 +0100

    Day one
    Basic way of selecting a folder , and create a new folder into this oneAjoutez des fichiers projet.

---
+++ DailyDevJourney/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DailyDevJourney
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SelectFolder = new Button();
+            FolderPath = new Label();
+            CreateFolder = new Button();
+            SuspendLayout();
+            // 
+            // SelectFolder
+            // 
+            SelectFolder.Location = new Point(83, 27);
+            SelectFolder.Name = "SelectFolder";
+            SelectFolder.Size = new Size(192, 46);
+            SelectFolder.TabIndex = 0;
+            SelectFolder.Text = " Select folder";
+            SelectFolder.UseVisualStyleBackColor = true;
+            SelectFolder.Click += SelectFolder_Click;
+            // 
+            // FolderPath
+            // 
+            FolderPath.AutoSize = true;
+            FolderPath.Location = new Point(87, 85);
+            FolderPath.Name = "FolderPath";
+            FolderPath.Size = new Size(0, 32);
+            FolderPath.TabIndex = 1;
+            FolderPath.Click += FolderPath_Click;
+            // 
+            // CreateFolder
+            // 
+            CreateFolder.Location = new Point(83, 379);
+            CreateFolder.Name = "CreateFolder";
+            CreateFolder.Size = new Size(658, 46);
+            CreateFolder.TabIndex = 2;
+            CreateFolder.Text = " Create a new folder here";
+            CreateFolder.UseVisualStyleBackColor = true;
+            CreateFolder.Click += CreateFolder_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CreateFolder);
+            Controls.Add(FolderPath);
+            Controls.Add(SelectFolder);
+            Name = "Form1";
+            Text = "DailyDevJourney";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button SelectFolder;
+        private Label FolderPath;
+        private Button CreateFolder;
+    }
+}
---
+++ DailyDevJourney/Form1.cs
@@ -0,0 +1,62 @@
+using MyForms = System.Windows.Forms;
+
+namespace DailyDevJourney
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            MyForms.FolderBrowserDialog folderBrowserDialog = new MyForms.FolderBrowserDialog();
+            folderBrowserDialog.InitialDirectory = "D:\\Dev\\DailyDevJourney";
+            MyForms.DialogResult Result = folderBrowserDialog.ShowDialog();
+
+            if (Result == MyForms.DialogResult.OK)
+            {
+                FolderPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void CreateFolder_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(FolderPath.Text))
+            {
+                string[] Folders = Directory.GetDirectories(FolderPath.Text);
+                int lastfoldernum = 0;
+                if (Folders.Length > 0)
+                {
+                    lastfoldernum = int.Parse(new DirectoryInfo(Folders[Folders.Length - 1]).Name);
+                }
+
+                lastfoldernum++;
+
+                Directory.CreateDirectory(FolderPath.Text + Path.DirectorySeparatorChar + lastfoldernum);
+            }
+            else
+            {
+                MessageBox.Show("No path selected yet");
+            }
+        }
+
+        private void FolderPath_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
