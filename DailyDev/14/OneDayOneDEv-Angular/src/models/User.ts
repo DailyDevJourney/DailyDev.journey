d4b0ea170c3da9b09f225746ea0b7a3c7ef00549
Alain <dailydev.journey@gmail.com>
2026-03-15 23:13:04 +0100

    update user avec Role

--- DailyDev/14/OneDayOneDEv-Angular/src/models/User.ts
+++ DailyDev/14/OneDayOneDEv-Angular/src/models/User.ts
@@ -2,10 +2,12 @@ export class User
 {
   Username: string;
   Password: string;
+  Role: string;
   constructor(username : string,password :string)
   {
     this.Username = username;
     this.Password = password;
+    this.Role = "USER";
 
   }
 
